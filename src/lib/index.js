82d61054e33866092a7509d8a2f61237b03bdc42
Revanth Kumar <revanth0212@gmail.com>
2022-08-16 17:39:59 -0500

    Created Dev Console lib functions

---
+++ src/lib/index.js
@@ -0,0 +1,9 @@
+const { describeMesh, getMesh, createMesh, updateMesh, deleteMesh } = require('./devConsole');
+
+module.exports = {
+	describeMesh,
+	getMesh,
+	createMesh,
+	updateMesh,
+	deleteMesh,
+};
