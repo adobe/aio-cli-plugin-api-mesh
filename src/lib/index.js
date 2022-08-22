de2581a70ac128d17d0d6777783e0e3e7a8ef81d
Revanth Kumar <revanth0212@gmail.com>
2022-08-22 10:38:39 -0500

    Removed unecessary file

--- src/lib/index.js
+++
@@ -1,9 +0,0 @@
-const { describeMesh, getMesh, createMesh, updateMesh, deleteMesh } = require('./devConsole');
-
-module.exports = {
-	describeMesh,
-	getMesh,
-	createMesh,
-	updateMesh,
-	deleteMesh,
-};
