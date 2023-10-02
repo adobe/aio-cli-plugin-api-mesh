994cfa558b0967080f798b88d8f11c51ec4b71a3
Deepty Thampy <dthampy@adobe.com>
2023-10-02 17:21:50 -0500

    fix: adding the logic to handle responseHeaders needed for includeMetadata

--- src/serverUtils.js
+++ src/serverUtils.js
@@ -3,9 +3,19 @@ const path = require('path');
 const makeCancellablePromise = require('make-cancellable-promise');
 const Timeout = require('await-timeout');
 const fetch = require('node-fetch');
+const LRUCache = require('lru-cache');
+const logger = require('./classes/logger');
 
 const MAX_TTL = 60 * 60 * 1000;
 const MIN_TTL = 60 * 1000;
+
+const headersCache = new LRUCache({
+	max: parseInt(process.env.CACHE_OPT_MAX || '500', 10),
+	ttl: parseInt(process.env.CACHE_HEADERS_TTL || '300000', 10), // 5 mins
+	dispose: (value, key, reason) => {
+		logger.info(`Removing headers for ${key} from headers cache because ${reason}`);
+	},
+});
 
 /**
  * Read .meshrc.json file stored in the mesh-artifact for a particular mesh
@@ -112,8 +122,186 @@ async function invokeRemoteFetch(url, params) {
 	}
 }
 
+function removeRequestHeaders(requesId) {
+	headersCache.delete(requesId);
+}
+
+/**
+ * This function loops through the mesh http details plugin and saves the response headers into an LRU cache
+ * @param meshHTTPDetails this is the object coming from the graphql-mesh plugin that contains information about the different sources
+ * @param requestId
+ */
+function prepSourceResponseHeaders(meshHTTPDetails, requestId) {
+	const mappedResponseHeaders = [];
+	//const requestId = request_context_1.requestContext.get('requestId');
+	meshHTTPDetails?.forEach(element => {
+		const headers = Object.entries(element.response.headers);
+		headers?.forEach(value => {
+			const header = value[0];
+			const headerValue = value[1];
+			const sourceName = element.sourceName;
+			const url = element.request.url;
+			const mappedResponseHeader = {
+				name: header.toLowerCase(),
+				source: sourceName,
+				values: [headerValue],
+			};
+			const sourceMarkedHeader = {
+				name:
+					//we want to return all the original headers from magento and source prefixed for everyone else
+					header.toLowerCase() !== 'cache-control' && !url.toLowerCase().includes('magento')
+						? `x-${element.sourceName}-${header.toLowerCase()}`
+						: header.toLowerCase(),
+				source: sourceName,
+				values: [headerValue],
+			};
+			mappedResponseHeaders.push(sourceMarkedHeader);
+			mappedResponseHeaders.push(mappedResponseHeader);
+		});
+		//Update the headers LRU cache
+		setSourceResponseHeaders(requestId, mappedResponseHeaders);
+	});
+}
+
+function setSourceResponseHeaders(requestId, responseHeaders) {
+	if (headersCache.has(requestId)) {
+		const currentResponseHeaders = headersCache.get(requestId);
+		const concatResponseHeaders = currentResponseHeaders.concat(responseHeaders);
+		headersCache.set(requestId, concatResponseHeaders);
+	} else {
+		headersCache.set(requestId, responseHeaders);
+	}
+}
+
+/**
+ *
+ * @param meshId
+ * @param requestId
+ * @param includeMetadata
+ * @param method
+ * @returns {Object.<string, string|string[]>}
+ */
+function processResponseHeaders(meshId, requestId, includeMetadata, method) {
+	const meshResponseConfig = getMeshResponseConfig(meshId);
+	const responseHeaders = headersCache.get(requestId);
+	const sourceResponseConfig = getSourceResponseHeaders(
+		responseHeaders,
+		meshId,
+		requestId,
+		includeMetadata,
+	);
+	processMeshResponseHeaders(meshResponseConfig, sourceResponseConfig, method, responseHeaders);
+}
+
+function getMeshResponseConfig(meshId) {
+	const meshConfig = readMeshConfig(meshId);
+	return meshConfig?.responseConfig;
+}
+
+function getSourceResponseHeaders(responseHeaders, meshId, requestId, includeMetadata) {
+	const sourceResponseHeaders = {};
+	const sourceResponseHeadersMap = new Map();
+
+	const currentWorkingDirectory = process.cwd();
+	const meshConfigPath = `${currentWorkingDirectory}/mesh-artifact/${meshId}/.meshrc.json`;
+
+	const meshConfig = require(meshConfigPath);
+	if (meshConfig) {
+		meshConfig.sources.forEach(source => {
+			if (source.responseConfig && source.responseConfig.headers) {
+				sourceResponseHeadersMap.set(source.name, source.responseConfig.headers);
+			}
+		});
+		responseHeaders?.forEach(element => {
+			const respArray = sourceResponseHeadersMap.get(element.source);
+			const lower = respArray?.map(element => {
+				return element.toLowerCase();
+			});
+			if (lower && lower.includes(element.name)) {
+				sourceResponseHeaders[element.name] = element.values;
+			}
+			if (includeMetadata) {
+				sourceResponseHeaders[element.name] = element.values;
+			}
+		});
+	} else {
+		logger.error(`No meshid ${meshId} found for requestId: ${requestId}`);
+		throw new Error(`No meshid ${meshId} found`, 'getSourceResponseHeaders', requestId);
+	}
+	//  }
+	return sourceResponseHeaders || {};
+}
+
+function processMeshResponseHeaders(
+	meshResponseConfig,
+	sourceResponseConfig,
+	method,
+	responseHeaders,
+) {
+	//Since we do not want any caching to happen on posts we need to make sure any cache-control headers
+	//are removed on posts. This includes at the mesh and source level
+	if (method.toLowerCase() === 'post') {
+		const removedCacheHeadersConfig = Object.fromEntries(
+			Object.entries(sourceResponseConfig).map(([k, v]) => [k.toLowerCase(), v]),
+		);
+		delete removedCacheHeadersConfig['cache-control'];
+		//if there are mesh headers we want to remove them as well
+		if (meshResponseConfig && meshResponseConfig.headers) {
+			const meshHeaders = Object.fromEntries(
+				Object.entries(meshResponseConfig.headers).map(([k, v]) => [
+					k.toLowerCase(),
+					v.toLowerCase(),
+				]),
+			);
+			delete meshHeaders['cache-control'];
+			return { ...meshHeaders } || {};
+		}
+		return { ...removedCacheHeadersConfig } || {};
+	}
+	//All other requests go through the usual path
+	if (meshResponseConfig && meshResponseConfig.headers) {
+		//make sure we are standardizing all the headers
+		const meshHeaders = Object.fromEntries(
+			Object.entries(meshResponseConfig.headers).map(([k, v]) => [
+				k.toLowerCase(),
+				v.toLowerCase(),
+			]),
+		);
+		return { ...meshHeaders, ...sourceResponseConfig } || {};
+	} else {
+		if (responseHeaders) {
+			const ccDirectives = getCacheControlDirectives(responseHeaders);
+			return { ...sourceResponseConfig, ...ccDirectives } || {};
+		}
+	}
+	return { ...sourceResponseConfig } || {};
+}
+
+/**
+ * Returns the lowest common denominator on all the sources cache-control headers
+ * @param responseHeaders
+ * @returns
+ */
+function getCacheControlDirectives(responseHeaders) {
+	const cacheControlHeaders = responseHeaders.filter(header => {
+		return header.name === 'cache-control';
+	});
+	const cacheControlDirectives = {};
+	cacheControlHeaders.forEach(header => {
+		const directives = header.values[0].split(',');
+		directives.forEach(directive => {
+			const [key, value] = directive.split('=');
+			cacheControlDirectives[key.trim()] = value.trim();
+		});
+	});
+	return cacheControlDirectives;
+}
+
 module.exports = {
 	readMeshConfig,
 	processContextConfig,
 	invokeRemoteFetch,
+	removeRequestHeaders,
+	prepSourceResponseHeaders,
+	processResponseHeaders,
 };
