b2a698a856985cc0350d9abb742f181198c243a9
Revanth <annavara@adobe.com>
2024-09-18 22:20:48 -0500

    Using mesh endpoint from action variables

--- src/templates/k6TestScript.js
+++ src/templates/k6TestScript.js
@@ -1,13 +1,11 @@
+/* eslint-disable no-undef */
 import http from 'k6/http';
 
 export default function () {
-	http.get(
-		'https://edge-sandbox-graph.adobe.io/api/3d995241-3497-43da-acf6-6f5045bc0eca/graphql?query={__schema{queryType{name}}}',
-		{
-			headers: {
-				'Content-Type': 'application/json',
-				'Connection': 'keep-alive',
-			},
+	http.get(`${__ENV.MESH_ENDPOINT}?query={__schema{queryType{name}}}`, {
+		headers: {
+			'Content-Type': 'application/json',
+			'Connection': 'keep-alive',
 		},
-	);
+	});
 }
