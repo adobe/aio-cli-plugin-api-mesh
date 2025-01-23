6116df2e9ea259cb3ddc42fb3dbd158ee10cc09b
mac <mac@macs-MacBook-Pro.local>
2025-01-24 01:51:08 +0530

    feat: Resolve secrets

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 const { spawn } = require('child_process');
+const { readSecretsFile } = require('./serverUtils');
 
 const runServer = (meshId, portNo) => {
 	const wranglerPath = `${__dirname}/../node_modules/.bin/wrangler`;
 	const indexFilePath = `${__dirname}/index.js`;
-	const commandArgs = ['dev', indexFilePath, '--var', `MESH_ID:${meshId}`, '--port', portNo];
+	const filePath = '.mesh';
+	const secrets = readSecretsFile(filePath);
+	const commandArgs = [
+		'dev',
+		indexFilePath,
+		'--var',
+		`MESH_ID:${meshId}`,
+		`Secret:${JSON.stringify(secrets)}`,
+		'--port',
+		portNo,
+	];
 
 	const wrangler = spawn(wranglerPath, commandArgs, {
 		stdio: 'inherit',
