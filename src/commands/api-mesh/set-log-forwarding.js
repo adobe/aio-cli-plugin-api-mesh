7953916685b6113cc9bb3653160588e9ebdc8dcd
Brasewel Noronha <100383619+brasewel@users.noreply.github.com>
2025-03-26 11:33:28 -0500

    Update src/commands/api-mesh/set-log-forwarding.js
    Co-authored-by: Jared Hoover <98363870+jhadobe@users.noreply.github.com>

--- src/commands/api-mesh/set-log-forwarding.js
+++ src/commands/api-mesh/set-log-forwarding.js
@@ -146,6 +146,6 @@ class SetLogForwardingCommand extends Command {
 SetLogForwardingCommand.description = `Sets the log forwarding destination for API mesh. 
 - Select a log forwarding destination: Choose from available options ( example : newrelic).
 - Enter the base URI: Provide the URI for the log forwarding service. Ensure it includes the protocol ( example : If the hosted region of New Relic account is U.S, the base URI can be 'https://log-api.newrelic.com/log/v1').
-- Enter the license key: Provide the INGEST-LICENSE API key type.`;
+- Enter the license key - Provide the INGEST-LICENSE API key type.`;
 
 module.exports = SetLogForwardingCommand;
