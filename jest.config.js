f67a64602742d7dc93412fb626db82c69aae8447
ajaz <ajaz@adobe.com>
2026-06-25 17:46:08 +0530

    fix: minor

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ const config = {
 	testEnvironment: 'node',
 	reporters: ['default', 'jest-junit'],
 	setupFilesAfterEnv: ['./jest.setup.js'],
-	testPathIgnorePatterns: ['/node_modules/', '\\.integration\\.test\\.js$'],
+	testPathIgnorePatterns: ['/node_modules/'],
 };
 
 module.exports = config;
